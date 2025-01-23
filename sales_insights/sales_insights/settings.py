e6fb4b502a350e44ecb467c08b66b61820209985
codeslashsagnik <csesagnik@gmail.com>
2025-01-23 15:18:31 +0530

    Data app added

--- sales_insights/sales_insights/settings.py
+++ sales_insights/sales_insights/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-rom decouple import config
+from decouple import config
 
 
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'data'
 ]
 
 MIDDLEWARE = [
